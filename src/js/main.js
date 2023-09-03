3941842f5f8e757e151e169856b31cb8a8398e54
Ilyar <kasyymov.ilyar@gmail.com>
2023-09-03 13:49:00 +0300

    add vanila-lazyload

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,9 @@
 import "../scss/styles.scss";
 
 // Import only the Bootstrap components we need
+import LazyLoad from "vanilla-lazyload";
+
 import "bootstrap";
 import "./menu";
+
+var lazyLoadInstance = new LazyLoad();
